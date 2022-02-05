976f0d2469a36a1e222c9d8a7eb1c14d8178597c
Douglas Berkley <douglasmberkley@gmail.com>
2022-02-05 10:46:22 +0900

    added URL endpoint and mapbox

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+// let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxi-api-qqi4hufipq-ew.a.run.app/predict'; // replace with your API endpoint
+
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG1iZjI5IiwiYSI6ImNqcjZ6dWYxMjJuM2Y0NHM3bW50eWJ5d2sifQ.qVo2MYwOoKlo6NWyJDXnXw';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
